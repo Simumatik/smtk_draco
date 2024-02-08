75dd132d7da69fe1062fe16dbc263b7ed57fe5e6
Marcus Barstorp <marcus.barstorp@simumatik.com>
2024-02-08 13:55:35 +0100

    build: bump version due to Test PyPI issue with a previous partial test upload
    [skip ci]

--- src/smtk_draco.pyx
+++ src/smtk_draco.pyx
@@ -1,7 +1,7 @@
 #cython: language_level=3
 #distutils: language = c++
 
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 # Include the implementation files to compile all the code into one extension
 include "common.pxi"
