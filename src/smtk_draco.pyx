1228e53e7336de1d1726673ffe93bc8268615f20
Marcus Barstorp <marcus.barstorp@simumatik.com>
2025-08-27 12:32:21 +0200

    ci: bump pypa/gh-action-pypi-publish to 1.12.4
    ci: remove the github_release job in the release workflow
    
    The action gh-action-pypi-publish takes care of attestations so we skip uploading artifacts to the Github Release

--- src/smtk_draco.pyx
+++ src/smtk_draco.pyx
@@ -1,7 +1,7 @@
 #cython: language_level=3
 #distutils: language = c++
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 # Include the implementation files to compile all the code into one extension
 include "common.pxi"
